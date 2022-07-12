2506c9974d50b9e92f1b6e1ba346fca774cb0346
bawkbak <hsiehbawkbak@gmail.com>
2022-07-12 16:50:29 +0800

    Feat: Add catkin build command & environment setting

---
+++ catkin_make_rpi.sh
@@ -0,0 +1,12 @@
+catkin build -w ~/drone_system/catkin_ws \
+    behavior_tree_msgs \
+    behavior_tree \
+    ros_tsp \
+    mavros_command \
+    drone_state_estimation \
+    drone_navigation \
+    drone_mavros \
+    avoidance \
+    global_planner \
+    local_planner \
+    safe_landing_planner
---
+++ environment.sh
@@ -0,0 +1,2 @@
+source /opt/ros/melodic/setup.bash
+source $HOME/drone_system/catkin_ws/devel/setup.bash
