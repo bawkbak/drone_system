c653740ddba0df36fa6b6e6407a92a7e044dfaa9
bawkbak <hsiehbawkbak@gmail.com>
2022-07-25 19:17:46 +0800

    Feat: New catkin script for xavier-nx / jetson nano

---
+++ catkin_make_nano.sh
@@ -0,0 +1,13 @@
+catkin build -w ~/drone_system/catkin_ws \
+    -j2 \
+    behavior_tree_msgs \
+    behavior_tree \
+    ros_tsp \
+    mavros_command \
+    drone_state_estimation \
+    drone_navigation \
+    drone_mavros \
+    avoidance \
+    global_planner \
+    local_planner \
+    safe_landing_planner
